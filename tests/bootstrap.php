1d42560b2b120285c7b0ed5a99de3449da3ef254
macro <869158081@qq.com>
2017-03-22 20:22:40 +0800

    初始化项目

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,8 +2,8 @@
 date_default_timezone_set('Asia/Shanghai');
 defined('APPLICATION_ENV') || define('APPLICATION_ENV', 'development');
 define('ROOT_PATH', dirname(__DIR__));
-define('APP_NAME', 'Blog');
-define('APP_PATH', ROOT_PATH . '/app/' . APP_NAME . '/');
+define('APP_NAME', 'Test');
+define('APP_PATH', __DIR__);
 require ROOT_PATH . '/vendor/autoload.php';
 $app = new \Polymer\Boot\Application();
 $app->startConsole();
