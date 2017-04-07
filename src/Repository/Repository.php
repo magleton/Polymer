0f20eba17a3d957af29bc8631742d6192925682e
macro <869158081@qq.com>
2017-04-07 20:02:08 +0800

    新增验证表达式组件!删除冗余的函数!

--- src/Repository/Repository.php
+++ src/Repository/Repository.php
@@ -84,21 +84,6 @@ class Repository extends EntityRepository
     }
 
     /**
-     * 将实体对象转换为数组
-     *
-     * @param mixed $entity   实体对象
-     * @param null $format 转换成的格式  json xml
-     * @return array|object|\Symfony\Component\Serializer\Normalizer\scalar
-     */
-    public function toArray($entity, $format = null)
-    {
-        $encoders = array(new XmlEncoder(), new JsonEncoder());
-        $normalizers = array(new ObjectNormalizer());
-        $serializer = new Serializer($normalizers, $encoders);
-        return $serializer->normalize($entity, $format);
-    }
-
-    /**
      * 给对象新增属性
      *
      * @param $propertyName
