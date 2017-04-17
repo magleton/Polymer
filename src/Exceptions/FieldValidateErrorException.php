bf461a29f7b56bd620f6c609f556ede371352a6d
macro <869158081@qq.com>
2017-04-17 19:21:10 +0800

    新增异常类!

---
+++ src/Exceptions/FieldValidateErrorException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * User: macro chen <chen_macro@163.com>
+ *
+ * Date: 2017/4/17
+ * Time: 19:15
+ */
+
+namespace Polymer\Exceptions;
+
+class FieldValidateErrorException extends \Exception
+{
+
+}
