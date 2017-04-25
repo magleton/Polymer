d863e40af9426a3d7b2a9b0990224c47d14747b5
macro <macro_fengye@163.com>
2017-04-25 08:38:10 +0800

    修改数据库链接缓存key!

--- src/Boot/Application.php
+++ src/Boot/Application.php
@@ -121,7 +121,7 @@ final class Application
         try {
             $dbConfig = $this->config('db.' . APPLICATION_ENV);
             $dbName = $dbName ?: current(array_keys($dbConfig));
-            $cacheKey = 'entityManager' . '.' . $this->config('db.' . APPLICATION_ENV . '.' . $dbName . '.emCacheKey', str_replace([':', DIRECTORY_SEPARATOR], ['', ''], APP_PATH)) . '.' . $dbName;
+            $cacheKey = 'em' . '.' . $this->config('db.' . APPLICATION_ENV . '.' . $dbName . '.emCacheKey', str_replace([':', DIRECTORY_SEPARATOR], ['', ''], APP_PATH)) . '.' . $dbName;
             if (isset($dbConfig[$dbName]) && $dbConfig[$dbName] && !$this->component($cacheKey)) {
                 $entityFolder = $entityFolder ?: ROOT_PATH . '/entity/Models';
                 $configuration = Setup::createAnnotationMetadataConfiguration([
