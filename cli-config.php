73574782525dce328457b778d27d263a99778be3
枫叶思源 <britton@126.com>
2021-07-13 22:08:21 +0800

    代码重构

--- cli-config.php
+++ cli-config.php
@@ -8,7 +8,6 @@ use Doctrine\Migrations\Tools\Console\Command\StatusCommand;
 use Doctrine\Migrations\Tools\Console\Command\VersionCommand;
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
-use Doctrine\ORM\Version;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Helper\HelperSet;
 use Symfony\Component\Console\Helper\QuestionHelper;
@@ -16,13 +15,13 @@ use Symfony\Component\Console\Helper\QuestionHelper;
 try {
     date_default_timezone_set('Asia/Shanghai');
     defined('APPLICATION_ENV') || define('APPLICATION_ENV', 'development');
-    const ROOT_PATH = __DIR__;
-    const APP_NAME = 'tests';
-    const APP_PATH = ROOT_PATH . '/' . APP_NAME . '/';
+    defined('ROOT_PATH') || define('ROOT_PATH', __DIR__);
+    defined('APP_NAME') || define('APP_NAME', 'tests');
+    defined('APP_PATH') || define('APP_PATH', ROOT_PATH . '/' . APP_NAME . '/');
     require ROOT_PATH . '/vendor/autoload.php';
     $app = new \Polymer\Boot\Application();
     try {
-        $app->startConsole();
+        $app->runConsole();
     } catch (Exception $e) {
     }
     $em = app()->db('db1', APP_PATH . '/Entity/Models');
@@ -31,7 +30,7 @@ try {
         'db' => new Doctrine\DBAL\Tools\Console\ConnectionProvider\SingleConnectionProvider($em->getConnection()),
         'dialog' => new QuestionHelper(),
     ));
-    $cli = new Application('Doctrine Command Line Interface', Version::VERSION);
+    $cli = new Application('Doctrine Command Line Interface', 'UNKNOWN');
     $cli->setCatchExceptions(true);
     $cli->setHelperSet($helperSet);
     $cli->addCommands([
