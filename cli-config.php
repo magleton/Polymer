a7116bacd46716c1d1fe3febd4ef65521a92eda7
britton <britton@126.com>
2021-07-15 09:58:19 +0800

    代码重构

--- cli-config.php
+++ cli-config.php
@@ -8,18 +8,19 @@ use Doctrine\Migrations\Tools\Console\Command\StatusCommand;
 use Doctrine\Migrations\Tools\Console\Command\VersionCommand;
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
+use Monolog\Logger;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Helper\HelperSet;
 
+date_default_timezone_set('Asia/Shanghai');
+defined('APPLICATION_ENV') || define('APPLICATION_ENV', 'development');
+defined('DS') || define('DS', DIRECTORY_SEPARATOR);
+defined('ROOT_PATH') || define('ROOT_PATH', __DIR__);
+defined('APP_NAME') || define('APP_NAME', 'tests');
+defined('APP_PATH') || define('APP_PATH', ROOT_PATH . DS . 'app' . DS . APP_NAME . DS);
+$loader = require __DIR__ . '/vendor/autoload.php';
+$app = new \Polymer\Boot\Application();
 try {
-    date_default_timezone_set('Asia/Shanghai');
-    defined('APPLICATION_ENV') || define('APPLICATION_ENV', 'development');
-    defined('DS') || define('DS', DIRECTORY_SEPARATOR);
-    defined('ROOT_PATH') || define('ROOT_PATH', __DIR__);
-    defined('APP_NAME') || define('APP_NAME', 'tests');
-    defined('APP_PATH') || define('APP_PATH', ROOT_PATH . DS . 'app' . DS . APP_NAME . DS);
-    $loader = require __DIR__ . '/vendor/autoload.php';
-    $app = new \Polymer\Boot\Application();
     $app->runConsole();
     $em = \Polymer\Boot\Application::getInstance()->db('db1', APP_PATH . 'Entity' . DS . 'Mapping');
     $helperSet = new HelperSet([
@@ -40,5 +41,5 @@ try {
     ConsoleRunner::addCommands($cli);
     $cli->run();
 } catch (Exception $e) {
-    print_r($e);
+    $app->get(Logger::class)->error($e);
 }
