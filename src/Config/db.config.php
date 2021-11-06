8704586187fdb97e719431b67097dc85cb927f3e
枫叶思源 <britton@126.com>
2021-11-06 16:19:31 +0800

    功能完善

--- src/Config/db.config.php
+++ src/Config/db.config.php
@@ -1,117 +1,61 @@
 <?php
 //数据库配置
-use Doctrine\DBAL\Sharding\PoolingShardConnection;
-use Doctrine\DBAL\Sharding\ShardChoser\MultiTenantShardChoser;
+use Doctrine\DBAL\Connections\PrimaryReadReplicaConnection;
 use Doctrine\ORM\Mapping\DefaultNamingStrategy;
 
 return [
     'db' => [
         // 开发模式
         'development' => [
-            "db1" => [
-                'wrapperClass' => PoolingShardConnection::class,
+            'db1' => [
+                'wrapperClass' => PrimaryReadReplicaConnection::class,
                 'namingStrategy' => DefaultNamingStrategy::class,
-                'shardChoser' => MultiTenantShardChoser::class,
                 'driver' => 'pdo_mysql',
-                'host' => '192.168.56.101',
-                'port' => 3306,
-                'user' => 'britton',
-                'password' => 'britton',
-                'dbname' => 'mydb',
-                "charset" => "UTF8",
-                'global' => [
-                    'driver' => 'pdo_mysql',
-                    'host' => '192.168.56.101',
-                    'port' => 3306,
-                    'dbname' => 'mydb',
-                    'user' => 'britton',
-                    'password' => 'britton',
-                    'charset' => 'UTF8'
+                'primary' => ['user' => '', 'password' => '', 'host' => '192.168.56.101', 'dbname' => 'mydb', 'port' => 3306, "charset" => "UTF8"],
+                'replica' => [
+                    ['user' => 'replica1', 'password', 'host' => '', 'dbname' => ''],
+                    ['user' => 'replica2', 'password', 'host' => '', 'dbname' => '']
                 ],
-                'shards' => [
-                    [
-                        'id' => 1,
-                        'driver' => 'pdo_mysql',
-                        'host' => '10.0.25.2',
-                        'user' => 'root',
-                        'password' => '111111',
-                        'dbname' => 'xiaofei',
-                        'charset' => 'UTF8',
-                        'port' => 3308
-                    ],
-                    [
-                        'id' => 2,
-                        'driver' => 'pdo_mysql',
-                        'host' => '10.0.25.2',
-                        'user' => 'root',
-                        'password' => '111111',
-                        'dbname' => 'test',
-                        'charset' => 'UTF8',
-                        'port' => 3308
-                    ]
-                ],
-                "useSimpleAnnotationReader" => false,
+                'useSimpleAnnotationReader' => false,
                 'emCacheKey' => 'abc',
             ],
-
-            "db2" => [
+            'db2' => [
+                'wrapperClass' => PrimaryReadReplicaConnection::class,
+                'namingStrategy' => DefaultNamingStrategy::class,
                 'driver' => 'pdo_mysql',
-                'host' => '127.0.0.1',
-                'port' => '3306',
-                'user' => 'root',
-                'password' => 'root',
-                'dbname' => 'xiaofei',
-                "charset" => "UTF8",
-                'sharding' => [
-                    'federationName' => 'my_database',
-                    'distributionKey' => 'customer_id',
+                'primary' => ['user' => '', 'password' => '', 'host' => '192.168.56.101', 'dbname' => 'mydb', 'port' => 3306, "charset" => "UTF8"],
+                'replica' => [
+                    ['user' => 'replica1', 'password', 'host' => '', 'dbname' => ''],
+                    ['user' => 'replica2', 'password', 'host' => '', 'dbname' => '']
                 ],
-                "useSimpleAnnotationReader" => false
-            ],
-            "db3" => [
-                'driver' => 'pdo_mysql',
-                'host' => '127.0.0.1',
-                'port' => '3306',
-                'user' => 'root',
-                'password' => 'root',
-                'dbname' => 'xiaofei',
-                "charset" => "UTF8",
-                'sharding' => [
-                    'federationName' => 'my_database',
-                    'distributionKey' => 'customer_id',
-                ],
-                "useSimpleAnnotationReader" => true
+                'useSimpleAnnotationReader' => true,
+                'emCacheKey' => 'abc',
             ]],
         // 生产模式
         'production' => [
-            "db1" => [
+            'db1' => [
+                'wrapperClass' => PrimaryReadReplicaConnection::class,
+                'namingStrategy' => DefaultNamingStrategy::class,
                 'driver' => 'pdo_mysql',
-                'host' => '127.0.0.1',
-                'port' => '3306',
-                'user' => 'root',
-                'password' => 'root',
-                'dbname' => 'xiaofei',
-                "charset" => "UTF8",
-                'sharding' => [
-                    'federationName' => 'my_database',
-                    'distributionKey' => 'customer_id',
+                'primary' => ['user' => '', 'password' => '', 'host' => '192.168.56.101', 'dbname' => 'mydb', 'port' => 3306, "charset" => "UTF8"],
+                'replica' => [
+                    ['user' => 'replica1', 'password', 'host' => '', 'dbname' => ''],
+                    ['user' => 'replica2', 'password', 'host' => '', 'dbname' => '']
                 ],
-                "useSimpleAnnotationReader" => false
+                'useSimpleAnnotationReader' => false,
+                'emCacheKey' => 'abc',
             ],
-            "db2" => [
+            'db2' => [
+                'wrapperClass' => PrimaryReadReplicaConnection::class,
+                'namingStrategy' => DefaultNamingStrategy::class,
                 'driver' => 'pdo_mysql',
-                'host' => 'localhost',
-                'port' => '3306',
-                'user' => 'username',
-                'password' => 'password',
-                'dbname' => 'production_dbname',
-                "charset" => "UTF8",
-                'sharding' => [
-                    'federationName' => 'my_database',
-                    'distributionKey' => 'customer_id',
+                'primary' => ['user' => '', 'password' => '', 'host' => '192.168.56.101', 'dbname' => 'mydb', 'port' => 3306, "charset" => "UTF8"],
+                'replica' => [
+                    ['user' => 'replica1', 'password', 'host' => '', 'dbname' => ''],
+                    ['user' => 'replica2', 'password', 'host' => '', 'dbname' => '']
                 ],
-                "useSimpleAnnotationReader" => true
-            ]
-        ],
+                'useSimpleAnnotationReader' => true,
+                'emCacheKey' => 'abc',
+            ]],
     ],
 ];
